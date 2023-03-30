0cc3d5e411d035651b87960efeac54cc37337757
Mike_Went <git@meew.me>
2023-03-30 21:12:59 +0400

    bump version

--- src/project/api.py
+++ src/project/api.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from project import routers
 from project.db import init_db, dispose_db
 
-app = FastAPI(title="tag-extractor", version="1.1")
+app = FastAPI(title="tag-extractor", version="1.2")
 app.include_router(routers.v1.router, prefix="/v1")
 
 
